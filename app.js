fb03a01a23616510b1a2c483bc46c791f2e55f03
Weldy <JBWeldy@osfhealthcare.org>
2023-05-10 21:26:26 +0100

    saml troubleshooting - 2

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/login', passport.authenticate('saml'));
 
 // Define the callback route
 app.post('/callback',
-  passport.authenticate('saml', { failureRedirect: '/lose' }),
+  //passport.authenticate('saml', { failureRedirect: '/lose' }),
   (req, res) => {
     console.log("in callback and redirecting to lose ");
     res.redirect('/secure');
