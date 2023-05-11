654b306d6446409862cc63791a6760c694517c81
Weldy <JBWeldy@osfhealthcare.org>
2023-05-11 05:52:48 +0100

    more testing -4

--- config/passport.js
+++ config/passport.js
@@ -18,8 +18,10 @@ passport.use(
         entryPoint: "https://mocksaml.com/saml/login",
         issuer: "devicetable.com",
         callbackUrl: "http://osf-dev-env.eba-kdcjyk3f.us-east-1.elasticbeanstalk.com/callback",
-        privateKey: fs.readFileSync("./certs/key.pem", "utf-8"),
-        cert: fs.readFileSync("./certs/cert.pem", "utf-8"),
+        //privateKey: fs.readFileSync("./certs/key.pem", "utf-8"),
+        //privateCert: fs.readFileSync("./certs/key.pem", "utf-8"),
+        //cert: fs.readFileSync("./certs/cert.pem", "utf-8") //,
+        /*
         authnContext: [
             "http://scheemas.microsoft.com/ws/2008/06/identity/authenticationmethod/password",
         ],
@@ -27,6 +29,7 @@ passport.use(
         signatureAlgorithm: "sha256",
         racComparison: "exact",
         audience: "https://adfs.blahblah.com/FederationMetadata/2007-06/FederationMetadata.xml",
+        */
       },
     function (profile, done) {
       console.log("assertions can KILLLLLLLLLLLLLLLL");
